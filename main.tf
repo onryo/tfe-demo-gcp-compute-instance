78df2f382518b9e8c6bcf07e6e8a84964b4a7166
George Watkins <1855559+onryo@users.noreply.github.com>
2019-11-21 23:03:01 -0800

    Fixed nat_ip output
    Changed assigned_nat_ip output attribute to nat_ip to reflect changes in GCP provider

--- main.tf
+++ main.tf
@@ -62,6 +62,6 @@ resource "google_compute_instance" "demo" {
 }
 
 output "external_ip" {
-  value = google_compute_instance.demo.network_interface[0].access_config[0].assigned_nat_ip
+  value = google_compute_instance.demo.network_interface[0].access_config[0].nat_ip
 }
 
